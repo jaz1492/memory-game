67ba57acc091d54aac51acccb93aa952ceca8f05
jaz1492 <zkloor@gmail.com>
2023-01-16 17:01:10 +0900

    1-16-23

---
+++ index.js
@@ -0,0 +1,6 @@
+const playBtn = document.body.querySelector(".play-button");
+const openingMenu = document.getElementById("opening-menu");
+function clearMenu(){
+    openingMenu.style.display="none";
+}
+// playBtn.onClick('event',()=>{openingMenu.display="none"});
