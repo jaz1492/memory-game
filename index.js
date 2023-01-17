7f938002b6c19c222ef761b236e0d644472cb0b2
jaz1492 <zkloor@gmail.com>
2023-01-17 16:42:03 +0900

    1-17-23

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const playBtn = document.body.querySelector(".play-button");
-const openingMenu = document.getElementById("opening-menu");
-function clearMenu(){
-    openingMenu.style.display="none";
-}
+// const playBtn = document.body.querySelector(".play-button");
+// const openingMenu = document.getElementById("opening-menu");
+// function clearMenu(){
+//     openingMenu.style.display="none";
+// }
 // playBtn.onClick('event',()=>{openingMenu.display="none"});
+const cards = document.body.querySelectorAll(".card");
+let selectedCards =[];
+for(const card of cards){
+card.addEventListener("click",()=>{
+    if(card.src.includes("card-back.png")){
+    card.src=`images/${card.parentElement.className}.png`}
+    else{
+        card.src="images/card-back.png"
+    }
+    selectedCards.push(card.parentElement.className);
+    if(selectedCards.length===2){
+        hasTwo();
+        setTimeout(()=>cards.src='images/card-back.png',1000);
+    }
+})};
+function hasTwo(){
+    setTimeout(()=>alert(`you selected ${selectedCards[0]} and ${selectedCards[1]}`),1000);
+}
