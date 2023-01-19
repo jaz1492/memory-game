7c582c2c84ba4a8cc84d6cb64595bec3d864ffa6
jaz1492 <zkloor@gmail.com>
2023-01-19 16:27:29 +0900

    added images

--- index.js
+++ index.js
@@ -4,23 +4,67 @@
 //     openingMenu.style.display="none";
 // }
 // playBtn.onClick('event',()=>{openingMenu.display="none"});
-const cards = document.body.querySelectorAll(".card");
+
+const suits=["Hearts", "Spades", "Diamonds", "Clubs"];
+const values =["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+const deck=[];
 let selectedCards =[];
+function clicker(){
+const cards = document.body.querySelectorAll(".card");
 for(const card of cards){
 card.addEventListener("click",()=>{
     if(card.src.includes("card-back.png")){
     card.src=`images/${card.parentElement.className}.png`}
-    else{
-        card.src="images/card-back.png"
-    }
-    selectedCards.push(card.parentElement.className);
+    selectedCards.push(card.parentElement.id);
     if(selectedCards.length===2){
         hasTwo();
-        for(const selectedCard of selectedCards){
-            document.body.querySelector("."+selectedCard).children[0].src="images/card-back.png";
+        setTimeout(()=>{
+        for(const card of cards){
+            console.log(card);
+            card.src="images/card-back.png";
         }
+        selectedCards=[];},1000)
     }
 })};
+}
 function hasTwo(){
-    setTimeout(()=>alert(`you selected ${selectedCards[0]} and ${selectedCards[1]}`),1000);
+    if(idNumber(selectedCards[0]).classList[0][0]===idNumber(selectedCards[1]).classList[0][0]){
+       setTimeout(()=> {
+        idNumber(selectedCards[0]).children[0].classList.add("card-hide");
+        idNumber(selectedCards[1]).children[0].classList.add("card-hide");
+        },1000);
+    }
+    else{
+        console.log(`error:${selectedCards[0]} and ${selectedCards[1]} don't match.`);
+    }
+}
+function cardMaker(){
+    // let num =1;
+    // for(let i=0;i<suits.length;i++){
+    //     for(let n=0;n<values.length;n++){       
+    //         deck.push(objFactory(`${values[n]}-of-${suits[i]}`,num));
+    //         num++
+    //         }
+    //     }
+        let num =1;
+        for(let i=0;i<suits.length;i++){
+            for(let n=0;n<values.length;n++){       
+                let newCard=document.createElement("div");
+                newCard.classList.add(`${values[n]}-of-${suits[i]}`);
+                newCard.id=num;
+                newCard.innerHTML='<img class="card" src="images/card-back.png" alt="">'
+                document.querySelector(".cards-container").appendChild(newCard);
+                num++
+                }
+            }
+            clicker();
+}
+function idNumber(i){
+    return document.getElementById(i);
+}
+function objFactory(name,idNum){
+    return{
+        class:name,
+        id:idNum
+    }
 }
